20731cc43cfd418a99074f885cf0b4a6504b3ef2
LAPTOP-KAAFL62K\User <haritody@gmail.com>
2025-03-28 22:10:23 +0700

    build: setup tailwind

--- apps/api/src/routers/sample.router.ts
+++ apps/api/src/routers/sample.router.ts
@@ -1,23 +1,23 @@
-import { SampleController } from '@/controllers/sample.controller';
-import { Router } from 'express';
 
-export class SampleRouter {
-  private router: Router;
-  private sampleController: SampleController;
+// import { Router } from 'express';
 
-  constructor() {
-    this.sampleController = new SampleController();
-    this.router = Router();
-    this.initializeRoutes();
-  }
+// export class SampleRouter {
+//   private router: Router;
+//   private sampleController: SampleController;
 
-  private initializeRoutes(): void {
-    this.router.get('/', this.sampleController.getSampleData);
-    this.router.get('/:id', this.sampleController.getSampleDataById);
-    this.router.post('/', this.sampleController.createSampleData);
-  }
+//   constructor() {
+//     this.sampleController = new SampleController();
+//     this.router = Router();
+//     this.initializeRoutes();
+//   }
 
-  getRouter(): Router {
-    return this.router;
-  }
-}
+//   private initializeRoutes(): void {
+//     this.router.get('/', this.sampleController.getSampleData);
+//     this.router.get('/:id', this.sampleController.getSampleDataById);
+//     this.router.post('/', this.sampleController.createSampleData);
+//   }
+
+//   getRouter(): Router {
+//     return this.router;
+//   }
+// }
