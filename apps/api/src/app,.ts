e30e00342497076d389439c71f9818226abed664
LAPTOP-KAAFL62K\User <haritody@gmail.com>
2025-05-13 22:02:08 +0700

    build: fix handle 405

--- apps/api/src/app,.ts
+++ apps/api/src/app,.ts
@@ -40,9 +40,30 @@ export default class App {
   }
 
   private configure(): void {
+    this.app.use(
+      cors({
+        origin: [
+          'http://localhost:3000',
+          'https://stayza-renting-property.vercel.app/',
+        ],
+        credentials: true,
+      }),
+    );
+    this.app.options('*', (req, res) => {
+      res.header(
+        'Access-Control-Allow-Origin',
+        'https://stayza-renting-property.vercel.app/',
+      );
+      res.header(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PUT, DELETE, OPTIONS',
+      );
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.header('Access-Control-Allow-Credentials', 'true');
+      res.sendStatus(204);
+    });
     this.app.use(express.json());
-    this.app.use(cors());
-    // this.app.use(urlencoded({ extended: true }));
+    this.app.use(urlencoded({ extended: true }));
   }
 
   private handleError() {
